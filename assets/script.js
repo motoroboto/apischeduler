dc7436832282c85eef542e78f2bf05aaf2b86887
motoroboto <robert.a.cullen@gmail.com>
2020-10-04 01:56:25 -0400

    save function is now working

--- assets/script.js
+++ assets/script.js
@@ -3,57 +3,35 @@ var currentTime = moment().format('LT');
 var currentDayEl = document.getElementById('currentDay');
 var currentTimeEl = document.getElementById('currentTime');
 var presentHour = parseInt(moment().hour());
-console.log('Hour ID is ', presentHour);
-
-
-    var tasks = {
-        '8': 'here are some things',
-        '9': '9AM',
-        '10': '10AM',
-        '11': '11AM',
-        '12': '12PM',
-        '13': '1PM',
-        '14': '2PM',
-        '15': '3PM',
-        '16': '4PM',
-        '17': '5PM',
-        '18': '6PM',
-        '19': '7PM',
-    };
-    console.log(tasks['08']);
-    console.log(tasks['13']);
-
-
-
 
 currentDayEl.textContent = currentDay;
 currentTimeEl.textContent = currentTime;
 
-
 $("textarea").each(function(){
     var hourBlock = parseInt($(this).attr('class'));
-    console.log(hourBlock);
     if (hourBlock < presentHour) {
-        console.log('past');
         $(this).addClass("past");
-        $(this).append(tasks[hourBlock]);
+        $(this).append(localStorage.task[hourBlock]);
+        var x = localStorage.getItem("task"+hourBlock)
+        $(this).append(x);
     }
     if (hourBlock == presentHour) {
-        console.log('present');
         $(this).addClass("present");
-        $(this).append(tasks[hourBlock]);
+        $(this).append(localStorage.task[hourBlock]);
+        var x = localStorage.getItem("task"+hourBlock)
+        $(this).append(x);
     }
     if (hourBlock > presentHour) {
-        console.log('future');
         $(this).addClass("future");
-        $(this).append(tasks[hourBlock]);
+        $(this).append(localStorage.task[hourBlock]);
+        var x = localStorage.getItem("task"+hourBlock)
+        $(this).append(x);
     }
     }
   );
 
-//   $("textarea").each(function(){
-//     var hourBlock = parseInt($(this).attr('class')); 
-//     console.log('task:', tasks[hourBlock]);
-//     $(this).append(tasks[hourBlock]);
-
-// });
+  $(".saveBtn").click(function(){
+    var taskNum = parseInt(this.id);
+    taskContent = $("body").find('#text-block'+ taskNum);
+    localStorage.setItem('task'+ taskNum, taskContent[0].value);   
+}); 
