630a5812bd17c30039f5afe7c5646252e5b849f7
motoroboto <robert.a.cullen@gmail.com>
2020-10-03 22:52:28 -0400

    loading data to text inputs

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,27 @@ var currentTimeEl = document.getElementById('currentTime');
 var presentHour = parseInt(moment().hour());
 console.log('Hour ID is ', presentHour);
 
+
+    var tasks = {
+        '8': 'here are some things',
+        '9': '9AM',
+        '10': '10AM',
+        '11': '11AM',
+        '12': '12PM',
+        '13': '1PM',
+        '14': '2PM',
+        '15': '3PM',
+        '16': '4PM',
+        '17': '5PM',
+        '18': '6PM',
+        '19': '7PM',
+    };
+    console.log(tasks['08']);
+    console.log(tasks['13']);
+
+
+
+
 currentDayEl.textContent = currentDay;
 currentTimeEl.textContent = currentTime;
 
@@ -15,18 +36,24 @@ $("textarea").each(function(){
     if (hourBlock < presentHour) {
         console.log('past');
         $(this).addClass("past");
+        $(this).append(tasks[hourBlock]);
     }
     if (hourBlock == presentHour) {
         console.log('present');
         $(this).addClass("present");
+        $(this).append(tasks[hourBlock]);
     }
     if (hourBlock > presentHour) {
         console.log('future');
         $(this).addClass("future");
+        $(this).append(tasks[hourBlock]);
     }
     }
   );
 
-  var hourlyToDo = window.localStorage.key(0);
-  console.log('hourlyToDo:', hourlyToDo)
-  console.log('key:', key)
+//   $("textarea").each(function(){
+//     var hourBlock = parseInt($(this).attr('class')); 
+//     console.log('task:', tasks[hourBlock]);
+//     $(this).append(tasks[hourBlock]);
+
+// });
