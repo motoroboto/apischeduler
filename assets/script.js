e710548d742c3e5f4065e6d8193c63597320ffa3
motoroboto <robert.a.cullen@gmail.com>
2020-10-05 22:28:23 -0400

    added clear button to remove all tasks saved in localstorage

--- assets/script.js
+++ assets/script.js
@@ -14,22 +14,23 @@ $("textarea").each(function(){
         $(this).addClass("past");
         var x = localStorage.getItem("task"+hourBlock)
         $(this).append(x);
-    }
-    if (hourBlock == presentHour) {
+    } if (hourBlock == presentHour) {
         $(this).addClass("present");
         var x = localStorage.getItem("task"+hourBlock)
         $(this).append(x);
-    }
-    if (hourBlock > presentHour) {
+    } if (hourBlock > presentHour) {
         $(this).addClass("future");
         var x = localStorage.getItem("task"+hourBlock)
         $(this).append(x);
-    }
-    }
-  );
+    }});
 
   $(".saveBtn").click(function(){
     var taskNum = parseInt(this.id);
     taskContent = $("body").find('#text-block'+ taskNum);
     localStorage.setItem('task'+ taskNum, taskContent[0].value);
 }); 
+
+$(".clearBtn").click(function(){
+    localStorage.clear();
+    location.reload();
+    });
